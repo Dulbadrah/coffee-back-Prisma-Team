2203eba0ac659721e2c93e206a2c3157ad4ff870
25LP4426 25LP4426 <25LP4426@pinecones-iMac.local>
2025-08-01 12:38:15 +0800

    usercreate nemse

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import userRouter from "./router/userProfile.router";
+import userRouter from "./router/user.router";
+
 
 const app = express();
 const port = 4200;
 app.use(express.json());
 
-app.use("/profile", userRouter);
+app.use("/user", userRouter);
 
-//
-//
 app.listen(port, async () => {
   console.log(`hello prisma port http://localhost:${port}`);
 });
