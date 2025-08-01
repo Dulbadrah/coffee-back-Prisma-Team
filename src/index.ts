c5e75e9bec756fc9197f0f7effd8ef52c8125cd0
25LP5306 25LP5306 <25LP5306@pinecones-iMac.local>
2025-08-01 13:03:27 +0800

    create get

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
-import userRouter from "./router/userProfile.router";
+import userRouterProfile from "./router/userProfile.router";
 
 const app = express();
 const port = 4200;
 app.use(express.json());
 
-app.use("/profile", userRouter);
+app.use("/profile", userRouterProfile);
+app.use("/profile", userRouterProfile);
+// app.use("/profile", userRouterProfile);
+// app.use("/profile", userRouterProfile);
+// app.use("/profile", userRouterProfile);
 
-//
-//
 app.listen(port, async () => {
   console.log(`hello prisma port http://localhost:${port}`);
 });
