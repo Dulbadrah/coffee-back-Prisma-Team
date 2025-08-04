614b3f69d67ba37fb67bc5d08b268d1fe3d3d3cb
Tsetsko <tseko0301@gmail.com>
2025-08-04 12:44:27 +0800

    donation router

--- src/controller/bankaccount/changeBankCard.controller.ts
+++ src/controller/bankaccount/changeBankCard.controller.ts
@@ -6,24 +6,28 @@ export const changeBankCard = async (req: Request, res: Response) => {
   // const { bankCardId } = req.params;
   const { country, firstName, lastName, cardNumber, expiryDate } = req.body;
 
-
   // if (!bankCardId || isNaN(Number(bankCardId))) {
   //   return res.status(400).json({ message: "bankCardId шаардлагатай ба тоон утга байх ёстой." });
   // }
 
   if (!userId || isNaN(Number(userId))) {
-    return res.status(400).json({ message: "userId шаардлагатай ба тоон утга байх ёстой." });
+    return res
+      .status(400)
+      .json({ message: "userId шаардлагатай ба тоон утга байх ёстой." });
   }
 
   if (!country || !firstName || !lastName || !cardNumber || !expiryDate) {
-    return res.status(400).json({ message: "Бүх шаардлагатай талбаруудыг бөглөнө үү." });
+    return res
+      .status(400)
+      .json({ message: "Бүх шаардлагатай талбаруудыг бөглөнө үү." });
   }
 
   const parsedExpiryDate = new Date(expiryDate);
   if (isNaN(parsedExpiryDate.getTime())) {
-    return res.status(400).json({ message: "expiryDate нь хүчинтэй огноо байх ёстой." });
+    return res
+      .status(400)
+      .json({ message: "expiryDate нь хүчинтэй огноо байх ёстой." });
   }
-
 
   try {
     const bankAccount = await prisma.bankCard.update({
@@ -35,12 +39,11 @@ export const changeBankCard = async (req: Request, res: Response) => {
         lastName,
         cardNumber,
         expiryDate: parsedExpiryDate,
-      
       },
     });
 
     res.status(200).json({ bankAccount });
   } catch (error) {
-    res.status(500).json({ message: "Серверийн алдаа гарлаа."  });
+    res.status(500).json({ message: "Серверийн алдаа гарлаа." });
   }
 };
--- src/controller/bankaccount/getBankCard.controller.ts
+++ src/controller/bankaccount/getBankCard.controller.ts
@@ -2,21 +2,20 @@ import { Response, Request } from "express";
 import { prisma } from "../../utils/prisma";
 
 export const getBankCard = async (req: Request, res: Response) => {
-const {userId}=req.params
+  const { userId } = req.params;
 
-if (!userId) {
-  return res.status(400).json({ message: "userId буруу байна." });
-}
+  if (!userId) {
+    return res.status(400).json({ message: "userId буруу байна." });
+  }
 
   try {
     const bankAccount = await prisma.bankCard.findUnique({
       where: { userId: Number(req.params.userId) },
     });
-    
+
     if (!bankAccount) {
       return res.status(404).json({ message: "Банкны карт олдсонгүй." });
     }
-
 
     res.status(200).json({ bankAccount });
   } catch (error) {
